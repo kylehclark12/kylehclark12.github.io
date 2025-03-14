13f7fa42d00b15caa8baf2745b61fa24e433031b
kylehclark12 <kyle.hunter.clark@gmail.com>
2025-03-14 14:59:32 -0400

    Update script.js

--- script.js
+++ script.js
@@ -1,8 +1,8 @@
 function displaydiv(divtdisp) {
 	document.getElementById("home").style.display = "none"
-  document.getElementById("forest").style.display = "none"
-  document.getElementById("desert").style.display = "none"
-  document.getElementById("savanna").style.display = "none"
+	document.getElementById("forest").style.display = "none"
+  	document.getElementById("desert").style.display = "none"
+  	document.getElementById("savanna").style.display = "none"
 
 	document.getElementById(divtdisp).style.display = "block"
 }
